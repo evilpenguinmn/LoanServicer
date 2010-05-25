f4cb10d414d1d381bfeb5239291fd10a6aaeb3e7
Michael Schwarz <mschwarz@technodane.com>
2010-05-25 12:16:34 -0500

    Worked on the UML a little. Still learning the tool.
    Tried BOUML because it generates more type of code -- didn't like the
    interface. Will stick with umbrello for now.

--- src/java/com/technodane/LoanServicer/Financial/Loan.java
+++ src/java/com/technodane/LoanServicer/Financial/Loan.java
@@ -10,6 +10,9 @@ public class Loan {
   // Fields
   //
 
+  private double loanAmount;
+  private double balance;
+  private double interestRate;
   
   //
   // Constructors
@@ -24,6 +27,54 @@ public class Loan {
   //
   // Accessor methods
   //
+
+  /**
+   * Set the value of loanAmount
+   * @param newVar the new value of loanAmount
+   */
+  private void setLoanAmount ( double newVar ) {
+    loanAmount = newVar;
+  }
+
+  /**
+   * Get the value of loanAmount
+   * @return the value of loanAmount
+   */
+  private double getLoanAmount ( ) {
+    return loanAmount;
+  }
+
+  /**
+   * Set the value of balance
+   * @param newVar the new value of balance
+   */
+  private void setBalance ( double newVar ) {
+    balance = newVar;
+  }
+
+  /**
+   * Get the value of balance
+   * @return the value of balance
+   */
+  private double getBalance ( ) {
+    return balance;
+  }
+
+  /**
+   * Set the value of interestRate
+   * @param newVar the new value of interestRate
+   */
+  private void setInterestRate ( double newVar ) {
+    interestRate = newVar;
+  }
+
+  /**
+   * Get the value of interestRate
+   * @return the value of interestRate
+   */
+  private double getInterestRate ( ) {
+    return interestRate;
+  }
 
   //
   // Other methods
